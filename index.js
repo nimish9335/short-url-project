cb0c47c504a04927e860bf97c1df8c08e8116f87
Nimish <np4447292@gmail.com>
2026-05-25 11:44:00 +0530

    ejs implement

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const urlRouter=require("./routes/url");
+const staticRouter=require("./routes/staticRouter");
 const URL = require("./models/urls");
 const app = express();
 const PORT = 8001;
@@ -10,11 +12,17 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/short-url")
   .then(() => console.log("MongoDB connected successfully."));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 // Middleware Configuration
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Mount the url creation routes
 app.use("/url", urlRouter);
+
+app.use("/", staticRouter);
 
 // Dynamic Redirection Handler and Analytic Logging
 app.get("/:shortId", async (req, res) => {
